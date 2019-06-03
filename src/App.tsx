bbc59ad4725dd8741623576ef821a3be6cd34aa7
Marcelo Andrade <andrademarcelo98@gmail.com>
2019-06-03 17:50:22 +0100

    - Added Redux Dev Tools Extension

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import FirstScreen from "./screens/FirstScreen";
 import { Provider } from "react-redux";
 import { combineReducers, createStore} from "redux";
 import FirstScreenReducer from "./store/FirstScreen/reducer";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const appReducers = combineReducers({
   field: FirstScreenReducer
 });
 
-const store = createStore(appReducers);
+const store = createStore(appReducers,composeWithDevTools());
 
 class App extends React.Component {
   render() {
